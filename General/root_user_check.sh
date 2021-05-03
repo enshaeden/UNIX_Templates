6fdd5629a8293c5e59e546e435ffb175a82659bb
enshaeden <38257460+enshaeden@users.noreply.github.com>
2021-05-03 11:45:53 -0700

    created root_user_check.sh function

---
+++ General/root_user_check.sh
@@ -0,0 +1,13 @@
+rootck ()
+{
+	echo "- $(date +%D-%H:%M:%S) - Starting user check" | tee -a "$LOG" 2>> "$ERRLOG"
+	echo "- $(date +%D-%H:%M:%S) -  Verifying that this script is being run as the root user" | tee -a "$LOG" 2>> "$ERRLOG"
+	echo "- $(date +%D-%H:%M:%S) -  ### Script will exit if it's not being run as root ###" | tee -a "$LOG" 2>> "$ERRLOG"
+	if [ "$(id -u)" = 0 ]; then
+		echo "- $(date +%D-%H:%M:%S) - User is root - continuing..." | tee -a "$LOG" 2>> "$ERRLOG"
+	else
+		exit 1
+	fi
+	echo "- $(date +%D-%H:%M:%S) - root user verification successful" | tee -a "$LOG" 2>> "$ERRLOG"
+}
+# End of Root user check
