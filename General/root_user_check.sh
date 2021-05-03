5a80945fd32c3628ae9243d581435e1285e4ce95
enshaeden <38257460+enshaeden@users.noreply.github.com>
2021-05-03 11:56:50 -0700

    fixed formatting error

--- General/root_user_check.sh
+++ General/root_user_check.sh
@@ -1,5 +1,4 @@
-rootck ()
-{
+rootck () {
 	echo "- $(date +%D-%H:%M:%S) - Starting user check" | tee -a "$LOG" 2>> "$ERRLOG"
 	echo "- $(date +%D-%H:%M:%S) -  Verifying that this script is being run as the root user" | tee -a "$LOG" 2>> "$ERRLOG"
 	echo "- $(date +%D-%H:%M:%S) -  ### Script will exit if it's not being run as root ###" | tee -a "$LOG" 2>> "$ERRLOG"
